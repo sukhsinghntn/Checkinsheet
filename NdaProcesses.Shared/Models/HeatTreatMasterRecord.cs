df9e85a7a6f1da79c7cd42b5a3083d9cc3cee8a4
sukhsinghntn <sukhsinghntn@gmail.com>
2025-10-03 10:10:18 -0400

    Handle non-numeric xValue entries in heat treat seed data

--- NdaProcesses.Shared/Models/HeatTreatMasterRecord.cs
+++ NdaProcesses.Shared/Models/HeatTreatMasterRecord.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using NDAProcesses.Shared.Serialization;
 
 namespace NDAProcesses.Shared.Models;
 
@@ -71,6 +72,7 @@ public class HeatTreatMasterRecord
     public string? ClipGrooveMeasureLocation { get; set; }
 
     [JsonPropertyName("xValue")]
+    [JsonConverter(typeof(FlexibleNullableDoubleConverter))]
     public double? XValue { get; set; }
 
     [JsonPropertyName("vValue")]
